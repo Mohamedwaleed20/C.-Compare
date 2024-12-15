1257784b8b27502da9f5ece1a3859e9d949b9ca5
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-12-15 22:49:48 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+int main()
+{
+   string a , b ;
+   cin >> a >> b ;
+   if(a<b)
+   {
+       cout << a ;
+   }
+   else
+   {
+       cout << b ;
+   }
+
+    return 0;
+}
